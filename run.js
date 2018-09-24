8e1c887af6b95577be33b3fd29d96b2842c7385b
Vitaly Kezlya <vitaly@kezlya.com>
2018-09-23 22:35:24 -0600

    Everything lowercase

--- run.js
+++ run.js
@@ -14,14 +14,13 @@ http.createServer(function(req, res) {
             body += chunk.toString();
         });
         req.on('end', () => {
+            let response ={}
 
             //Hive object from request payload
             let hive = JSON.parse(body)
 
-            let response ={}
-
             //Loop through ants and give orders
-            for (let antId in hive.Ants) {
+            for (let antId in hive.ants) {
                 let random = Math.floor(Math.random() * 4);
                 response[antId] = {
                   "act":actions[random],
